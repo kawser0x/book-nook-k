89e532df4c4a20b734d366111e6fcabfd1c31a4f
kawser0x <kawserswe@gmail.com>
2026-08-18 03:02:30 +0600

    implement add room function using post operation

---
+++ src/app/add-room/layout.jsx
@@ -0,0 +1,8 @@
+export const metadata = {
+  title: "StudyNook - Add Room",
+  description: "Find & Book Private Study Spaces",
+};
+
+export default function AddRoomLayout({ children }) {
+  return children;
+}
---
+++ src/app/rooms/layout.jsx
@@ -0,0 +1,8 @@
+export const metadata = {
+  title: "StudyNook - All Rooms",
+  description: "Find & Book Private Study Spaces",
+};
+
+export default function RoomsLayout({ children }) {
+  return children;
+}
